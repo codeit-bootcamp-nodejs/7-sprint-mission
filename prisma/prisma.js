5a04a79f4f3dd2ad368b08baccda738d9d85d4cb
CodeinHyuk <jih34602050@gmail.com>
2025-12-05 01:38:15 +0000

    feat: Initial project setup, package installation, and base schema definition

---
+++ prisma/prisma.js
@@ -0,0 +1,22 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const { PrismaClient } = require("../generated/prisma/index.js");
+
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pkg from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  console.error("DATABASE_URL is not defined in environment variables.");
+  process.exit(1);
+}
+const pool = new pkg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
+export { prisma };
