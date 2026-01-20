0966ccd94e9bba908f6aa2afabcd658977de3660
qjdqjd1234 <qjdqjd1234@gmail.com>
2026-01-20 06:12:37 +0000

    미션6

--- sprint-mission-6.sql
+++ sprint-mission-6.sql
@@ -179,4 +179,4 @@ FROM order_details od
 JOIN pizzas p ON od.pizza_id = p.pizza_id
 JOIN pizza_types pt ON p.pizza_type_id = pt.pizza_type_id
 GROUP BY pt.name
-ORDER BY total_revenue DESC; ;
+ORDER BY total_revenue DESC;
