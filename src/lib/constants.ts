3fe04c34cb968df4a3fba811f41fe17666be9054
CodeinHyuk <jih34602050@google.com>
2026-03-17 14:31:06 +0900

    chore: AWS RDS 인스턴스 연결 및 프로덕션 DB 환경 설정

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const DATABASE_URL = process.env.DATABASE_URL as string;
+export const DATABASE_URL = process.env.DATABASE_URL || '';
 export const PORT = parseInt(process.env.PORT || '3000', 10);
 export const PUBLIC_PATH = './public';
 export const STATIC_PATH = '/public';
 export const ACCESS_TOKEN_COOKIE_NAME = 'access-token';
 export const REFRESH_TOKEN_COOKIE_NAME = 'refresh-token';
-export const JWT_SECRET = process.env.JWT_SECRET as string;
+export const JWT_SECRET = process.env.JWT_SECRET || '';
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY || '';
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || '';
+export const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+export const AWS_REGION = process.env.AWS_REGION || '';
+export const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME || '';
