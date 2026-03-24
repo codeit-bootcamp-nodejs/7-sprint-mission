342aff5b5720acb05bfd8592217cd35a7308994b
ktmgw486 <ktmgw486@gmail.com>
2026-03-24 09:44:55 +0900

    feat: 불필요한 폴더 제거

--- sprint11/pandamarket-sprint11-deploy-main/sprint-mission-10-main/ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apps : [{
-  name: "pandamarket", 
-  script: 'build/main.js',
-  instances : 0	  
-  }]		
-};
--- sprint11/pandamarket-sprint11-deploy-main/sprint-mission-10-main/src/main.ts
+++
@@ -1,6 +0,0 @@
-import server from './app';
-import { PORT } from './lib/constants';
-
-server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
