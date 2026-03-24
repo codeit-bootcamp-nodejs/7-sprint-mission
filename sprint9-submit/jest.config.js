174473be319ef858736f58735b5e0a5bc133f91e
Bora Lee <dltngus1317@gmail.com>
2026-03-24 09:32:23 +0900

    chore: sprint11 작업 환경 정리 및 .gitignore 추가

--- sprint9-submit/jest.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-  modulePaths: ['<rootDir>/src'],
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
-};
