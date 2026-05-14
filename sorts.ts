2e54767338137dab75687985523da99abe1bc7e5
codes-gy <podojjang_kr@naver.com>
2026-05-14 10:44:55 +0900

    feat: 정렬 알고리즘 구현

--- sorts.ts
+++ sorts.ts
@@ -27,11 +27,44 @@ function insertionSort(arr: number[]): number[] {
 }
 
 function mergeSort(arr: number[]): number[] {
+    if (arr.length <= 1) return arr;
 
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
-
+function merge(left : number[], right: number[]) : number[] {
+    const result : number[] = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+    return [...result, ...left.slice(i), ...right.slice(j)];
+}
 function quickSort(arr: number[]): number[] {
+    if (arr.length <= 1) return arr;
 
+    const pivot = arr[arr.length - 1];
+    const left : number[] = [];
+    const right: number[] = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 console.log(selectionSort([64, 25, 12, 22, 11])); // [11, 12, 22, 25, 64]
